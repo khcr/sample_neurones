de9e825b8e0bea8a06b07cf37cafbbefd71f0ff0
khcr <kocher.ke@gmail.com>
2012-11-10 14:02:01 +0100

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleNeurones::Application.config.secret_token = 'de8daecfb06b3e0e6da14db452352c722eb5514ae2f861798f98d81bc66592178758b6ab0641c24e0306f32e668d10aa6c22e1a80b2086c64f36e86c9ea0504d'
